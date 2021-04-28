b025c53640ddd3b83abd6e6e452c33fd6ac4dd46
Natsuno Tsukuda <natsuno-t@ws20is007.naist.jp>
2021-04-28 18:12:58 +0900

    download csv

--- content_scripts.js
+++ content_scripts.js
@@ -1,6 +1,14 @@
 let Data = { URL: "" };
 let id = 0;
 const syllabus_url_pattern = /https:\/\/syllabus.naist.jp\/[a-z]*\/preview_detail/g;
+const timeTable = {
+  1: { start: "9:20", end: "10:50" },
+  2: { start: "11:00", end: "12:30" },
+  3: { start: "13:30", end: "15:00" },
+  4: { start: "15:10", end: "16:40" },
+  5: { start: "16:50", end: "18:20" },
+  6: { start: "18:30", end: "20:00" },
+};
 
 window.addEventListener("load", (e) => {
   chrome.tabs.getSelected((tab) => {
@@ -12,16 +20,21 @@ window.addEventListener("load", (e) => {
       var classYear = document.getElementById("class-year");
       var classURL = document.getElementById("class-url");
       var classDetail = document.getElementById("class-detail");
+      var content =
+        "Subject,Start Date,Start Time,End Date,End Time,Place,Description\n";
+      var fileName = "test";
       chrome.tabs.executeScript(
         id,
         {
           file: "dataGetScripts.js",
         },
         function (results) {
-          console.log(results);
+          //show the class data in the popup
           className.innerText = results[0]["授業名"];
+          fileName = results[0]["授業名"];
           classYear.innerText = "年度: " + results[0]["年度"];
-		  classURL.innerText = Data.URL
+          classURL.innerText = Data.URL;
+
           for (el in results[0]["授業日程"]) {
             classDetail.innerHTML +=
               "<div>" +
@@ -34,11 +47,53 @@ window.addEventListener("load", (e) => {
               ", 場所: " +
               results[0]["授業日程"][el]["講義室"] +
               "</div><br/>";
+
+            //csv用データ作成
+            content +=
+              results[0]["授業名"] +
+              "(" +
+              el +
+              ")," +
+              results[0]["授業日程"][el]["日付"] +
+              "/" +
+              results[0]["年度"] +
+              "," +
+              timeTable[results[0]["授業日程"][el]["時限"]]["start"] +
+              "," +
+              results[0]["授業日程"][el]["日付"] +
+              "/" +
+              results[0]["年度"] +
+              "," +
+              timeTable[results[0]["授業日程"][el]["時限"]]["end"] +
+              "," +
+              results[0]["授業日程"][el]["講義室"] +
+              "," +
+              Data.URL +
+              "\n";
           }
         }
       );
 
       //Add to google Calender
+      var addButton = document.getElementById("add-button");
+      addButton.onclick = () => {
+        console.log("Click!");
+        console.log(fileName);
+        const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+        const blob = new Blob([bom, content], { type: "text/csv" });
+
+        if (window.navigator.msSaveBlob) {
+          window.navigator.msSaveBlob(blob, fileName + ".csv");
+          window.navigator.msSaveOrOpenBlob(blob, fileName + "test.csv");
+        } else {
+          const url = window.URL.createObjectURL(blob);
+          addButton.href = url;
+          addButton.download = fileName+".csv";
+          setTimeout(() => {
+            window.URL.revokeObjectURL(url);
+          }, 1000);
+        }
+      };
     } else {
       var className = document.getElementById("class-name");
       var addButton = document.getElementById("add-button");
@@ -46,33 +101,4 @@ window.addEventListener("load", (e) => {
       addButton.disabled = true;
     }
   });
-  // 1. Load the JavaScript client library.
-  gapi.load("client", start);
 });
-
-function add_google_calender() {}
-
-function start() {
-  // 2. Initialize the JavaScript client library.
-  gapi.client
-    .init({
-      apiKey: "AIzaSyC8qU43-dMwK4Pi_xlf_Jj1hCbCdX-qOtw",
-      // Your API key will be automatically added to the Discovery Document URLs.
-      discoveryDocs: ["https://people.googleapis.com/$discovery/rest"],
-    })
-    .then(function () {
-      // 3. Initialize and make the API request.
-      return gapi.client.people.people.get({
-        resourceName: "people/me",
-        "requestMask.includeField": "person.names",
-      });
-    })
-    .then(
-      function (response) {
-        console.log(response.result);
-      },
-      function (reason) {
-        console.log("Error: " + reason.result.error.message);
-      }
-    );
-}
