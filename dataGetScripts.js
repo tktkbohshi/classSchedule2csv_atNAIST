632ee5dd5ea41d25433006a2f7390e62e795b717
Natsuno Tsukuda <natsuno-t@ws20is007.naist.jp>
2021-05-17 18:52:42 +0900

    #1 正規表現と条件分岐でhtml構造の違うページを読み分け

--- dataGetScripts.js
+++ dataGetScripts.js
@@ -10,7 +10,12 @@ var year = title_splited[0].replace(/[^0-9]/g, "");
 //get time table and place
 class_dict = { 授業日程: {} };
 var table_body = document.getElementsByClassName("tbl01 mB20")[4].children[0];
-trs = Array.from(table_body.children);
+var trs = Array.from(table_body.children);
+//html表記違い対策
+if (!/^[0-9]{1,2}\/[0-9]{1,2}$/.test(trs[1].children[1].innerText)) {
+  table_body = document.getElementsByClassName("tbl01 mB20")[5].children[0];
+  trs = Array.from(table_body.children);
+}
 trs.map((el, i) => {
   if (i > 0) {
     class_dict["授業日程"][i] = {};
